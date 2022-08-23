b7bb1e9df4c091e004ffe29d20cfbdab5a2dc4ab
goldebrodkk <jake.kelly3564@gmail.com>
2022-08-23 15:38:41 +0100

    increase font size for ArticleContents body

--- src/Components/ArticleContents.jsx
+++ src/Components/ArticleContents.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const ArticleContents = ({ singleArticle }) => {
 
     return (
-        <>
+        <div>
         <h1>{singleArticle.title}</h1>
         <h2>{singleArticle.author}</h2>
         <Link to={`/articles/${singleArticle.topic}`}><h3>{singleArticle.topic}</h3></Link>
-        <article>{singleArticle.body}</article>
+        <article className="articleContents__body">{singleArticle.body}</article>
         <p>{singleArticle.created_at.slice(0, 10)}</p>
         <p>&#128077; {singleArticle.votes}</p>
-        </>
+        </div>
     )
 }
 
